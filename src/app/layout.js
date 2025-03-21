d42c25cfdcdf2e2efbd4c267ac602d8eb77abdb3
MagdaIG <magda.inalaf@gmail.com>
2025-03-20 22:05:41 -0600

    Add tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/globals.css";
 import CustomCursor from "../components/CustomCursor";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -20,10 +21,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className="h-full">
+        <head>
+
+        </head>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased h-full bg-[--background] text-[--text-color] relative`}>
         {children}
         <CustomCursor /> {/* 🔹 Cursor en todas las páginas */}
         </body>
+        <GoogleAnalytics gaId="G-ER9LCZNFE2" />
         </html>
     );
 }
